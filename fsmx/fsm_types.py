633e28bbbbd026505c8d7fa05641b8631d1676db
Sheñey <sheneyby2010@gmail.com>
2026-04-18 17:25:34 -0600

    fix: a import mistake with the news features

--- fsmx/fsm_types.py
+++ fsmx/fsm_types.py
@@ -1,6 +1,7 @@
 
 from typing import Literal, TypedDict, Iterable
 from collections.abc import Callable
+from enum import Enum
 
 type Action[C] = Callable[[C], None]
 type Guard[C] = Callable[[C], bool]
@@ -17,11 +18,21 @@ type TransitionMap[S, E, C] = dict[
 type Hook[C] = Callable[[C], None]
 type HookMap[S, C] = dict[S, list[Hook[C]]]
 
-type RenderMethod = Literal['console', 'file', 'diagram']
-
-class TransitionModel[S, E, C](TypedDict):
+class TransitionModel[S: Enum, E: Enum, C](TypedDict):
     from_state: S | Iterable[S]
     event: E
     to_state: S
     func: Action[C]
     guard: Guard[C] | None
+
+type VisualizePart = Literal['transitions', 'hooks']
+type RequiredParts = VisualizePart | Literal['any'] | Literal['all']
+class StateMachinePayload[S: Enum, E: Enum, C](TypedDict):
+    class _HookSummary(TypedDict):
+        on_enter: HookMap[S, C]
+        on_exit: HookMap[S, C]
+        before_each_transition: list[Hook[C]]
+        after_each_transition: list[Hook[C]]
+
+    transitions: TransitionMap[S, E, C]
+    hooks: _HookSummary
